249658e54264e3fce1b77c079c47559532c728f8
Marco Streng <marcostreng@me.com>
2024-10-25 12:15:27 +0200

    fix: add missing field

--- packages/client/src/types/gql.ts
+++ packages/client/src/types/gql.ts
@@ -1,8 +1,8 @@
 import {
-  UseMutationOptions,
-  UseQueryOptions,
   useMutation,
+  UseMutationOptions,
   useQuery,
+  UseQueryOptions,
 } from '@tanstack/react-query';
 import { fetcher } from '../lib/index';
 export type Maybe<T> = T | null;
@@ -303,7 +303,7 @@ export type RestaurantsQuery = {
     name: string;
     address?: string | null;
     position: { __typename?: 'Position'; lat: number; lng: number };
-    ratings: Array<{ __typename?: 'Rating'; value: number }>;
+    ratings: Array<{ __typename?: 'Rating'; userId: string; value: number }>;
   }>;
   groups: Array<{ __typename?: 'Group'; id: string; name: string }>;
 };
@@ -497,6 +497,7 @@ export const RestaurantsDocument = `
       lng
     }
     ratings {
+      userId
       value
     }
   }
